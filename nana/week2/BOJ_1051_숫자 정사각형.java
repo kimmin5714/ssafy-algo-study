b87bf2a628f325a241a0456958b6d4af106bd0aa
xw2nana <139676399+xw2nana@users.noreply.github.com>
2023-08-02 11:30:11 +0900

    BOJ_1051_숫자 정사각형.java

--- nana/week2/BOJ_1051_숫자 정사각형.java
+++ nana/week2/BOJ_1051_숫자 정사각형.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_17143 {
+public class BOJ_1051 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
