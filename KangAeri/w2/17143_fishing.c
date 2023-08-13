340e554a1930e7917b10bae9602c98daa9663e4d
KangAeri <87957253+KangAeri@users.noreply.github.com>
2023-08-13 23:30:36 +0900

    Solved_ 17143_fishing.c

--- KangAeri/w2/17143_fishing.c
+++ KangAeri/w2/17143_fishing.c
@@ -1,117 +1,118 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int mov[5][2] = {{0,0},{-1,0},{1,0},{0,1},{0,-1}};
+int mov[4][2] = {{-1,0},{1,0},{0,1},{0,-1}};
 
 typedef struct shark{
-	int v;
-	int d;
-	int s;
+   int v;//velocity
+   int d;//direction
+   int s;//size
 }shark;
 
 struct shark state[100][100];
-//struct shark deck[100][100];
+struct shark deck[100][100];
+
 int main(){
-	int r,c,m,got=0;
-	int vel=0, dir=0, size=0;
-	int i,j,ni,nj,k,p,t;
-	struct shark** deck;
-	//struct shark** state;
-	scanf("%d %d %d", &r, &c, &m);
-	deck = (struct shark**)malloc(sizeof(struct shark*)*r);
-	for(i=0;i<r;i++){
-		deck[i] = (struct shark*)malloc(sizeof(struct shark)*c);
-		for(j=0;j<c;j++){
-			deck[i][j].s=0;
-		}
-	} //alloc mem & init
-	
-	for (k= 0; k <m; k++){
-		scanf("%d %d %d %d %d", &i, &j, &vel, &dir,&size);
-		deck[i-1][j-1].v = vel;
-		deck[i-1][j-1].d = dir;
-		deck[i-1][j-1].s = size;		 
-	}
+   int r,c,m,got=0;
+   int vel=0, dir=0, size=0;
+   int i,j,ni,nj;
+   int k,p,t;
 
-	for(k=0;k<c;k++){
-		//copy;
-		for(i=0;i<r;i++){
-			for(j=0;j<c;j++){
-				state[i][j] = deck[i][j];
-				deck[i][j].s=0;
-			}
-		}
+   scanf("%d %d %d", &r, &c, &m);
+ 
+   for(i=0;i<r;i++){
+      for(j=0;j<c;j++){
+         deck[i][j].s=0;
+         deck[i][j].d=-1;
+         deck[i][j].v=0;
+      }
+   }
+   
+   for (k= 0; k <m; k++){
+      scanf("%d %d %d %d %d", &i, &j, &vel, &dir,&size);
+      deck[i-1][j-1].v = vel;
+      deck[i-1][j-1].d = dir-1;
+      deck[i-1][j-1].s = size;     
 
-		//catch shark
-		for(p=0;p<r;p++){
-			if(state[p][k].s>0){
-				got+=state[p][k].s;
-				state[p][k].s=0;
-				break;
-			}
-		} 
+   }
+   
+   for(k=0;k<c;k++){
+      //copy to state
+      for(i=0;i<r;i++){
+         for(j=0;j<c;j++){
+            state[i][j] = deck[i][j];
+            deck[i][j].s=0;
+         }
+      }
+
+      //catch shark
+      for(p=0;p<r;p++){
+         if(state[p][k].s>0){
+            got+=state[p][k].s;
+            state[p][k].s=0;
+            break;
+         }
+      } 
+   
+      //move sharks
+      for(i=0;i<r;i++){
+         for(j=0;j<c;j++){
+            if(state[i][j].s<=0){
+            	continue;
+            }       
+           ni= i + (mov[state[i][j].d][0] * state[i][j].v);
+		 nj= j + (mov[state[i][j].d][1] * state[i][j].v);
+		   dir=state[i][j].d;
+		    
+           if(ni<0){
+              ni*=-1;
+              if((ni/(r-1))%2==0){
+                 ni%=(r-1);
+                 dir=1;
+              } else{
+                 ni=r-1-(ni%(r-1));
+                 dir=0;
+              }
+           } else if(ni>=r){
+              if((ni/(r-1))%2==0){
+                 ni%=(r-1);
+                 dir=1;
+              } else{
+                 ni=r-1-(ni%(r-1));
+                 dir=0;
+              }
+           }
+             
+           //col mov  
+           if(nj<0){
+              nj*=-1;
+              if((nj/(c-1))%2==0){
+                 nj%=(c-1);
+                 dir=2;
+              } else{
+                 nj=c-1-(nj%(c-1));
+                 dir=3;
+              }      
+           } else if(nj>=c){
+              if((nj/(c-1))%2==0){
+                 nj%=(c-1);
+                 dir=2;
+              } else{
+                 nj=c-1-(nj%(c-1));
+                 dir=3;
+              } 
+           }
+             
+           if(deck[ni][nj].s <state[i][j].s){
+               deck[ni][nj] = state[i][j];
+               deck[ni][nj].d = dir;
+           }
+           state[i][j].s = 0;            
+         }//end for:j
+      }//end for:i
 	
-		//move sharks
-		for(i=0;i<r;i++){
-			for(j=0;j<c;j++){
-				if(state[i][j].s>0){
-					ni= i + mov[state[i][j].d][0] * state[i][j].v;
-					if(ni<0){
-						t =(-1*ni);
-						if((t/(r-1))%2==0){
-							ni=t%(r-1);
-						} else{
-							ni=r-1-t%(r-1);
-						}
-						
-					} else if(ni>=r){
-						t = (ni-(r-1));
-						if((t/(r-1))%2==0){
-							ni=r-1-(t%(r-1));
-						} else{
-							ni=t%(r-1);
-						}
-					}
-					nj= j + mov[state[i][j].d][1] * state[i][j].v;
-					if(nj<0){
-						t =(-1*nj);
-						if((t/(c-1))%2==0){
-							nj=t%(c-1);
-						} else{
-							nj=c-1-t%(c-1);
-						}
-						
-						if(((-1*nj)/(c-1))%2==0){
-							nj=t;
-						} else{
-							nj=c-t;
-						}
-					} else if(nj>=c){
-						t = (nj-(c-1));
-						if((t/(c-1))%2==0){
-							nj=c-1-(t%(c-1));
-						} else{
-							nj=t%(c-1);
-						}
-					}
-					if(deck[ni][nj].s <state[i][j].s){
-						deck[ni][nj].s = state[i][j].s;
-						deck[ni][nj].d = state[i][j].d;
-						deck[ni][nj].v = state[i][j].v;		
-					}
-					state[i][j].s = 0;		
-				} //end if :existanceChk
-			}//end for:j
-		}//end for:i
-		//copy to state
-	} //end for:k
-	
-	printf("%d\n",got);
-	for(i=0;i<r;i++){
-		free(deck[i]);
-		//free(state[i]);
-	}
-	free(deck);
-	//free(state);
-	return 0;
+   } //end for:k
+   
+   printf("%d\n",got);
+
+   return 0;
 }
